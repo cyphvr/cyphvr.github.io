9b5a377bae43009ef76a07b3edf7ae8d3f136009
sylcvh <112990173+sylcvh@users.noreply.github.com>
2026-02-03 03:01:30 -0500

    .

--- api/status.js
+++ api/status.js
@@ -1,14 +1,11 @@
-// Vercel serverless function to proxy status requests
 const http = require('http');
 
 module.exports = async (req, res) => {
-  // Only allow GET requests
   if (req.method !== 'GET') {
     res.status(405).json({ error: 'Method not allowed' });
     return;
   }
 
-  // Proxy the request to the HTTP backend
   http.get('http://cypher.hype.surf:10001', (proxyRes) => {
     let data = '';
     proxyRes.on('data', chunk => data += chunk);
